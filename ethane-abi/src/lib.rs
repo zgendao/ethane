600944871f3f3e2e79ef3d233a5481a07a775fed
Mark Melczer <melczer7@gmail.com>
2021-03-30 08:04:59 +0200

    WIP encode decode.

--- ethane-abi/src/lib.rs
+++ ethane-abi/src/lib.rs
@@ -1,7 +1,28 @@
+use ethereum_types::{Address, U256};
+
+pub enum ParameterType {
+    Address(Address),
+    Uint256(U256),
+}
+
+//impl ParameterType {
+//    pub fn encode(&self) -> serde_json {
+//    }
+//
+//    pub fn decode(raw: &serde_json::Value) -> Self {
+//    }
+//}
+//
+
+pub fn pad_to_32_bytes(value: &[u8]) -> [u8; 32] {
+    assert!(value.len() < 32, format!("Cannot pad to 32 bytes, input is too long ({} bytes)", value.len()));
+    let mut padded = [0u8; 32];
+    let diff = 32 - value.len();
+    padded[diff..].copy_from_slice(value);
+    padded
+}
+
 #[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+mod test {
+
 }
