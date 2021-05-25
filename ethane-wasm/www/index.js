c60e9e8fd15e450bc0a116e40f7a94867f3da026
Mark Melczer <melczer7@gmail.com>
2021-05-25 14:37:14 +0200

    Remove `www` from tracked files.

--- ethane-wasm/www/index.js
+++
@@ -1,5 +0,0 @@
-import { RequestArguments, Web3 } from "ethane-wasm";
-
-const args = RequestArguments.new("eth_syncing", []);
-const web3 = Web3.new("http://localhost:8545");
-web3.call(args).then((val) => document.write(val));
